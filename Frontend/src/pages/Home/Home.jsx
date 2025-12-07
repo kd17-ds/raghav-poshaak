7d8495a40398b098342592a2ef3bbd419a047ff2
Divyansh Sharma <shan17div@gmail.com>
2025-12-07 12:30:38 +0530

    Shifted to nextjs properly

--- Frontend/src/pages/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import Hero from "./Hero.jsx";
-
-export default function Home() {
-    return (
-        <div className="bg-primary rounded-b-2xl w-full h-full flex justify-center py-18 px-11">
-            <Hero />
-        </div>
-    );
-}
